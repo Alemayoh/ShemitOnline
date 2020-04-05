1a61c306c94b9423ebd8c315c57c7dcbb1ff1649
MichealSebre <msebre@gmail.com>
2020-04-05 01:54:02 -0400

    Additional front end pages

--- src/main/java/com/codepow/shemitonline/dao/AccountDAO.java
+++ src/main/java/com/codepow/shemitonline/dao/AccountDAO.java
@@ -1,7 +1,6 @@
 package com.codepow.shemitonline.dao;
 
 import org.hibernate.Session;
-<<<<<<< HEAD
 import org.hibernate.SessionFactory;
 import com.codepow.shemitonline.entity.Account;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @Repository
 public class AccountDAO {
- 
+	 
     @Autowired
     private SessionFactory sessionFactory;
  
@@ -20,19 +19,7 @@ public class AccountDAO {
         return session.find(Account.class, userName);
     }
  
-=======
-import org.springframework.transaction.annotation.Transactional;
-
-import com.codepow.shemitonline.entity.Account;
-
-@Transactional
-public class AccountDAO {
-  
-	public Account findAccount(String userName) {
-		Session session=this.sessionFactory.getCurrentSessioin();
-		return session.find(Account.class,userName);
-	}
->>>>>>> f667d88dd5d07c32da434eb67194b3407da3e50d
 }
+
 
   
