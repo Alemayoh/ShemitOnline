c0a1584bb40f04e211d3d0b45c258a10be59acdc
MichealSebre <msebre@gmail.com>
2020-04-05 00:45:21 -0400

    Create AccountDao
    Account DAO

--- src/main/java/com/codepow/shemitonline/dao/AccountDAO.java
+++ src/main/java/com/codepow/shemitonline/dao/AccountDAO.java
@@ -1,6 +1,13 @@
 package com.codepow.shemitonline.dao;
 
+
+
+@Transactional
 public class AccountDAO {
   
+	public Account findAccount(String userName) {
+		Session session=this.sessionFactory.getCurrentSessioin();
+		return session.find(Account.class,userName);
+	}
 }
   
