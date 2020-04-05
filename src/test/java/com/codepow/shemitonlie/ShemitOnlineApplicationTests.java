0502ca8cd0eef625e734847e3527626ec16d8d58
alex <solomonalex592@gmail.com>
2020-04-04 20:54:41 -0500

    Alemayoh Commit

---
+++ src/test/java/com/codepow/shemitonlie/ShemitOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codepow.shemitonlie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShemitOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
