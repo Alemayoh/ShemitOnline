216b3b9a49e77a0884bd847c6f69e796e3847d07
MichealSebre <msebre@gmail.com>
2020-04-04 23:52:14 -0400

    Account
    Account

--- src/main/java/com/codepow/shemitonline/entity/Account.java
+++ src/main/java/com/codepow/shemitonline/entity/Account.java
@@ -1,5 +1,14 @@
 package com.codepow.shemitonline.entity;
 
-public class Account {
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="Accounts")
+public class Account {
+     
+	   private static final long serialVersionUID=-2054386655979281969L;
+	   
+	   public static final String ROLE_MANAGER="MANAGER";
+	   public static final String ROLE_EMPLOYEE="EMPLOYEE";
 }
