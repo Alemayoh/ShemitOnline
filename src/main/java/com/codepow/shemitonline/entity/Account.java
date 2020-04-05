55d54eddf20f1d79f08d7b87b026a394c0526b57
MichealSebre <msebre@gmail.com>
2020-04-04 23:53:18 -0400

    Update Account
    cretae account table

--- src/main/java/com/codepow/shemitonline/entity/Account.java
+++ src/main/java/com/codepow/shemitonline/entity/Account.java
@@ -1,6 +1,8 @@
 package com.codepow.shemitonline.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
@@ -11,4 +13,56 @@ public class Account {
 	   
 	   public static final String ROLE_MANAGER="MANAGER";
 	   public static final String ROLE_EMPLOYEE="EMPLOYEE";
+	   
+	   @Id
+	    @Column(name = "User_Name", length = 20, nullable = false)
+	    private String userName;
+	 
+	    @Column(name = "Encryted_Password", length = 128, nullable = false)
+	    private String encrytedPassword;
+	 
+	    @Column(name = "Active", length = 1, nullable = false)
+	    private boolean active;
+	 
+	    @Column(name = "User_Role", length = 20, nullable = false)
+	    private String userRole;
+	 
+	    public String getUserName() {
+	        return userName;
+	    }
+	 
+	    public void setUserName(String userName) {
+	        this.userName = userName;
+	    }
+	 
+	    public String getEncrytedPassword() {
+	        return encrytedPassword;
+	    }
+	 
+	    public void setEncrytedPassword(String encrytedPassword) {
+	        this.encrytedPassword = encrytedPassword;
+	    }
+	 
+	    public boolean isActive() {
+	        return active;
+	    }
+	 
+	    public void setActive(boolean active) {
+	        this.active = active;
+	    }
+	 
+	    public String getUserRole() {
+	        return userRole;
+	    }
+	 
+	    public void setUserRole(String userRole) {
+	        this.userRole = userRole;
+	    }
+	 
+	    @Override
+	    public String toString() {
+	        return "[" + this.userName + "," + this.encrytedPassword + "," + this.userRole + "]";
+	    }
+	 
+	
 }
